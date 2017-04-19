6015bbf1f457b164ad5c453da8e3e91c164d1208
gaoyx <melo@gaoyuexiang.cn>
2017-04-19 16:46:43 +0800

    init

---
+++ src/main/java/cn/gaoyuexiang/lostAndFound/LostAndFoundUserApplication.java
@@ -0,0 +1,12 @@
+package cn.gaoyuexiang.lostAndFound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFoundUserApplication.class, args);
+	}
+}
