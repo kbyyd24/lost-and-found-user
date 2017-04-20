2ea06a6b05e37d84563dfa69f2c007e95cc27bbb
gaoyx <melo@gaoyuexiang.cn>
2017-04-20 13:10:35 +0800

    update

--- src/main/java/cn/gaoyuexiang/lostAndFound/dao/UserRepo.java
+++ src/main/java/cn/gaoyuexiang/lostAndFound/dao/UserRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends CrudRepository<User, Long> {
+public interface UserRepo extends CrudRepository<User, String> {
 
   User findByUsernameOrEmail(String username, String email);
 
