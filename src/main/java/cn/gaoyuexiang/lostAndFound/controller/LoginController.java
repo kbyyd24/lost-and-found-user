b8a62efbbac0c2b8b490f7d38de6171de8181dd1
gaoyx <melo@gaoyuexiang.cn>
2017-04-21 15:18:24 +0800

    finish api get /user/login/:username

--- src/main/java/cn/gaoyuexiang/lostAndFound/controller/LoginController.java
+++ src/main/java/cn/gaoyuexiang/lostAndFound/controller/LoginController.java
@@ -16,20 +16,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static cn.gaoyuexiang.lostAndFound.model.dto.enums.LoginMsg.MSG_NOT_COMPLETE;
 import static cn.gaoyuexiang.lostAndFound.model.dto.enums.LoginMsg.USERNAME_PASSWORD_NOT_MATCH;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 @UserController
 public class LoginController {
 
   private LoginService loginService;
+  private Map<String, HttpStatus> stateMap;
 
   @Autowired
   public LoginController(LoginService loginService) {
     this.loginService = loginService;
+    stateMap = new HashMap<>();
+    stateMap.put("online", OK);
+    stateMap.put("offline", NOT_FOUND);
+    stateMap.put("unauthorized", UNAUTHORIZED);
   }
 
   @PostMapping("login")
@@ -47,5 +53,12 @@ public class LoginController {
   @ResponseStatus(UNAUTHORIZED)
   public Message handleUnauthorizedException() {
     return new Message(USERNAME_PASSWORD_NOT_MATCH.getMsg());
+  }
+
+  @GetMapping("login/{username}")
+  public ResponseEntity<Message> checkState(@PathVariable String username,
+                                            @RequestHeader("user-token") String token) {
+    String state = loginService.checkState(username, token);
+    return new ResponseEntity<>(new Message(state), stateMap.get(state));
   }
 }
---
+++ src/test/java/cn/gaoyuexiang/lostAndFound/controller/LoginControllerTestForCheckState.java
@@ -0,0 +1,84 @@
+package cn.gaoyuexiang.lostAndFound.controller;
+
+import cn.gaoyuexiang.lostAndFound.model.dto.Message;
+import cn.gaoyuexiang.lostAndFound.service.LoginService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.springframework.http.HttpMethod.GET;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class LoginControllerTestForCheckState {
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  @MockBean
+  private LoginService loginService;
+
+  @Test
+  public void should_response_200_when_user_is_online() throws Exception {
+    String online = "online";
+    String token = "token";
+    String username = "username";
+    given(loginService.checkState(eq(username), eq(token))).willReturn(online);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.add("user-token", token);
+    HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+    ResponseEntity<Message> responseEntity =
+        restTemplate.exchange(String.format("/user/login/%s", username),
+            GET,
+            httpEntity,
+            Message.class);
+    assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
+    assertThat(responseEntity.getBody().getMsg(), is(online));
+  }
+
+  @Test
+  public void should_response_401_when_token_is_not_match() throws Exception {
+    String unauthorized = "unauthorized";
+    String token = "token";
+    String username = "username";
+    given(loginService.checkState(eq(username), eq(token))).willReturn(unauthorized);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.add("user-token", token);
+    HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
+    ResponseEntity<Message> entity =
+        restTemplate.exchange(String.format("/user/login/%s", username),
+            GET,
+            httpEntity,
+            Message.class);
+    assertThat(entity.getStatusCode(), is(HttpStatus.UNAUTHORIZED));
+    assertThat(entity.getBody().getMsg(), is(unauthorized));
+  }
+
+  @Test
+  public void should_response_404_when_user_is_offline() throws Exception {
+    String offline = "offline";
+    String token = "token";
+    String username = "username";
+    given(loginService.checkState(eq(username), eq(token))).willReturn(offline);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.add("user-token", token);
+    HttpEntity<Object> httpEntity = new HttpEntity<>(httpHeaders);
+    ResponseEntity<Message> entity =
+        restTemplate.exchange(String.format("/user/login/%s", username),
+            GET,
+            httpEntity,
+            Message.class);
+    assertThat(entity.getStatusCode(), is(HttpStatus.NOT_FOUND));
+    assertThat(entity.getBody().getMsg(), is(offline));
+  }
+}
