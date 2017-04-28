07b46dda7f51008769a807e8cf11156a27b4fdac
gaoyx <melo@gaoyuexiang.cn>
2017-04-28 15:39:53 +0800

    [#1][refactor] instead MockitoJUnitRunner and @Mock in UserInfoService test cases

--- src/test/java/cn/gaoyuexiang/lostAndFound/service/impl/UserInfoServiceImplTestForCreateInfo.java
+++ src/test/java/cn/gaoyuexiang/lostAndFound/service/impl/UserInfoServiceImplTestForCreateInfo.java
@@ -1,27 +1,31 @@
 package cn.gaoyuexiang.lostAndFound.service.impl;
 
+import cn.gaoyuexiang.lostAndFound.LostAndFoundUserApplication;
 import cn.gaoyuexiang.lostAndFound.dao.UserInfoRepo;
 import cn.gaoyuexiang.lostAndFound.model.dto.UserInfoDTO;
 import cn.gaoyuexiang.lostAndFound.model.persistence.UserInfo;
-import cn.gaoyuexiang.lostAndFound.service.IdCreatorService;
 import cn.gaoyuexiang.lostAndFound.service.UserInfoService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LostAndFoundUserApplication.class)
 public class UserInfoServiceImplTestForCreateInfo {
 
+  @Autowired
   private UserInfoService userInfoService;
 
-  @Mock
+  @MockBean
   private UserInfoRepo userInfoRepo;
 
   @Before
