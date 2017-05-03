1e21834221c5931209a474812de5cfde68e8d377
gaoyx <melo@gaoyuexiang.cn>
2017-05-03 14:37:40 +0800

    [#3][#4] user map to instead of switch

--- src/main/java/cn/gaoyuexiang/lostAndFound/controller/EmailController.java
+++ src/main/java/cn/gaoyuexiang/lostAndFound/controller/EmailController.java
@@ -8,12 +8,12 @@ import cn.gaoyuexiang.lostAndFound.service.EmailVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -22,30 +22,26 @@ public class EmailController {
 
   private EmailVerificationService emailVerificationService;
 
+  private Map<EmailVerifyMsg, HttpStatus> responseMap;
+
   @Autowired
   public EmailController(EmailVerificationService emailVerificationService) {
     this.emailVerificationService = emailVerificationService;
+    this.responseMap = new HashMap<>();
+    this.responseMap.put(EmailVerifyMsg.SUCCESS, OK);
+    this.responseMap.put(EmailVerifyMsg.UNAUTHORIZED, UNAUTHORIZED);
+    this.responseMap.put(EmailVerifyMsg.EMAIL_NOT_FOUND, NOT_FOUND);
+    this.responseMap.put(EmailVerifyMsg.EMAIL_ENABLED, CONFLICT);
+    this.responseMap.put(EmailVerifyMsg.TOKEN_TIMEOUT, GONE);
   }
 
   @PutMapping(path = "verification/application", headers = "user-token")
   public ResponseEntity<Message> apply(@PathVariable("email") String email,
-                                       @RequestHeader(name = "user-token") String token) {
-    try {
-      EmailVerifyMsg result = emailVerificationService.apply(email, token);
-      switch (result) {
-        case SUCCESS:
-          return new ResponseEntity<>(new Message(result.name()), OK);
-        case UNAUTHORIZED:
-          return new ResponseEntity<>(new Message(result.name()), UNAUTHORIZED);
-        case EMAIL_NOT_FOUND:
-          return new ResponseEntity<>(new Message(result.name()), NOT_FOUND);
-        case EMAIL_ENABLED:
-          return new ResponseEntity<>(new Message(result.name()), CONFLICT);
-      }
-    } catch (MessagingException e) {
-      return new ResponseEntity<>(new Message("something wrong"), INTERNAL_SERVER_ERROR);
-    }
-    return null;
+                                       @RequestHeader(name = "user-token") String token) throws MessagingException {
+    EmailVerifyMsg result = emailVerificationService.apply(email, token);
+    HttpStatus responseStatus = this.responseMap.compute(result,
+        (emailVerifyMsg, httpStatus) -> httpStatus == null ? INTERNAL_SERVER_ERROR : httpStatus);
+    return  new ResponseEntity<>(new Message(result.name()), responseStatus);
   }
 
   @PutMapping(path = "verification", headers = {"username", "user-token"})
@@ -55,18 +51,14 @@ public class EmailController {
                                         @RequestHeader("username") String username) {
     EmailVerifyMsg verifyResult =
         emailVerificationService.verify(username, userToken, email, emailToken.getToken());
-    switch (verifyResult) {
-      case SUCCESS:
-        return new ResponseEntity<>(new Message(verifyResult.name()), OK);
-      case TOKEN_TIMEOUT:
-        return new ResponseEntity<>(new Message(verifyResult.name()), GONE);
-      case UNAUTHORIZED:
-        return new ResponseEntity<>(new Message(verifyResult.name()), UNAUTHORIZED);
-      case EMAIL_NOT_FOUND:
-        return new ResponseEntity<>(new Message(verifyResult.name()), NOT_FOUND);
-      default:
-        return new ResponseEntity<>(new Message("unknown result"), NOT_FOUND);
-    }
+    HttpStatus responseStatus = this.responseMap.compute(verifyResult,
+        (emailVerifyMsg, httpStatus) -> httpStatus == null ? INTERNAL_SERVER_ERROR : httpStatus);
+    return new ResponseEntity<>(new Message(verifyResult.name()), responseStatus);
   }
 
+  @ExceptionHandler(MessagingException.class)
+  @ResponseStatus(INTERNAL_SERVER_ERROR)
+  public Message handleMessagingException() {
+    return new Message("send email failed");
+  }
 }
--- src/main/java/cn/gaoyuexiang/lostAndFound/service/EmailVerificationService.java
+++ src/main/java/cn/gaoyuexiang/lostAndFound/service/EmailVerificationService.java
@@ -1,11 +1,13 @@
 package cn.gaoyuexiang.lostAndFound.service;
 
 import cn.gaoyuexiang.lostAndFound.enums.EmailVerifyMsg;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.MessagingException;
 
 public interface EmailVerificationService {
 
+  @Transactional(rollbackFor = MessagingException.class)
   EmailVerifyMsg apply(String email, String token) throws MessagingException;
 
   EmailVerifyMsg verify(String username, String userToken, String email, String verifyToken);
